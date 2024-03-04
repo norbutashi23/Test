5e9174b2f85ba14cdb8d5b25dfe91cd901947fd1
norbutashi23 <02230108.cst@rub.edu.bt>
2024-03-04 16:40:50 +0530

    second commit

--- Exercise2.py
+++ Exercise2.py
@@ -11,14 +11,14 @@ students_dict[name] = {"age": age, "grade": grade}
 print("Information added successfully!")
 
 # function to display the students info
-search_students = input("Enter the student's name to search")
+search_students = input("Enter the student's name to search: ")
 if search_students in students_dict:
     print(f"Student found: {name}, Age: {students_dict[name]['age']}, Grade: {students_dict[name]['grade']}")
 else:
     print("Information not found")
     
 # Function to remove a student info
-remove_students = input("Enter the student's name to remove")
+remove_students = input("Enter the student's name to remove or enter to skip: ")
 if remove_students in students_dict:
     students_list.remove(name)
     del students_dict[name]
