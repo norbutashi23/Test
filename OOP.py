706952ac06a16827fb4b5a843ad35cc115a2257e
norbutashi23 <02230108.cst@rub.edu.bt>
2024-05-22 23:22:59 +0530

    first commit

---
+++ OOP.py
@@ -0,0 +1,17 @@
+class Dog:
+    def __init__(self,name,color,age):
+        self.name = name
+        self.color = color
+        self.age = age
+Dawa = Dog("Dawa Koto","Black",3)
+Nima = Dog("Nima Sintu","Red",4)
+Kelzang = Dog("Kelzang Angel","white",11)
+print(Dawa.name)
+print(Dawa.color)
+print(Dawa.age)
+print(Nima.name)
+print(Nima.color)
+print(Nima.age)
+print(Kelzang.name)
+print(Kelzang.color)
+print(Kelzang.age)
