ae095d1af5dd4b4f200b6d1244238d3c09a18ea7
norbutashi23 <02230108.cst@rub.edu.bt>
2024-05-26 14:19:23 +0530

    first commit

---
+++ DeskManager(LAP5).py
@@ -0,0 +1,30 @@
+from queue import Queue
+
+#initialize patient queue
+patient_queue = Queue()
+ 
+ 
+ #Implementing a menu driven program
+while true:
+    print("Patient Registration and Appointment System")
+    print("1. Register Patient")
+    print("2. Remove Patient after meeting Doctor")
+    print("3. Display Patient Queue")
+    print("4. Exit")
+ 
+choice = int(input("Enter Your Choice(Just enter the option number):"))
+
+
+ 
+
+
+    
+    
+
+    
+    
+   
+    
+    
+    
+        
---
+++ dash.py
@@ -0,0 +1,10 @@
+def update(x: int):
+    return x + 1
+
+def main():
+    x = 1
+    while x < 11:
+        x = update(x)
+        print("wakanda")
+        
+main()
